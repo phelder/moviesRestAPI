250e6ec155b9455b1edf93090861bfc3c9b684e3
Helder Pereira <phelder@gmail.com>
2024-05-16 22:24:17 +0200

    added env PORT

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const movieRouter = require("./routes/movieRouter");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
